1e3fe9abcada9951b7d5b44542f9871745308a8b
syed farhan <syedfarhan9034@gmail.com>
2019-07-03 18:06:48 +0530

    some minor database changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database : 'mongodb://localhost:27017/meanauth',
+    database : 'mongodb+srv://farhan:farhan123@demo-nhoid.mongodb.net/test?retryWrites=true&w=majority',
     secretKey : 'mykey'
 }
